098ea5525ff6bbe55f1ee633e79faa62c26f3977
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-01 18:53:30 +0300

    feat: count cats function completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
 const CustomError = require("../extensions/custom-error")
 
 module.exports = function countCats(matrix) {
+  let counter = 0
+  matrix.forEach(element => {
+    element.forEach(element => {
+      if (element === '^^') counter++
+    })
+  })
+  return counter
 }

8e1e80af4941f199bd7ef9c073a9f1185c5442f7
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-15 12:41:35 +0300

    feat: dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  return members.map(name => {
+    if (typeof name !== 'string') return ''
+    return name.trim().toUpperCase().split('')[0]
+  }).sort().join('')
+}

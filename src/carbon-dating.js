75abec39e150045429686ba41d75e15f2cafefbb
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-15 12:28:39 +0300

    feat: carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error")
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY= 15
+const HALF_LIFE_PERIOD= 5730
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string'
+    || sampleActivity === undefined
+    || typeof (+sampleActivity) !== 'number'
+    || isNaN(+sampleActivity)
+    || (+sampleActivity) <= 0
+    || (+sampleActivity) > 15) {
+      return false
+  }
+  let k = 0.693 / HALF_LIFE_PERIOD
+  return Math.ceil(Math.log2(MODERN_ACTIVITY/+sampleActivity) / k)
+}
